cae2cfb7459503b25202f656d0043c81271c532b
marcarro <hmarroyom@gmail.com>
2024-12-01 14:08:53 -0600

    Progress

--- evidencia2.h
+++ evidencia2.h
@@ -9,28 +9,55 @@
  * Esta estructura representa una edge con un peso
  */
 struct Edge {
-    int src, dest, weight;
     Edge(int s, int d, int w) : src(s), dest(d), weight(w) {}
+
+    int src; ///< Fuente de la Edge
+	int dest; ///< Destino de la Edge
+	int weight; ///< Peso de la Edge
 };
 
-//Estructura para un punto
+/** 
+ * Estructura para un punto
+ */
 struct Point {
-    int x, y;
     Point(int _x = 0, int _y = 0) : x(_x), y(_y) {}
+    int x; ///< Coordenada x
+	int y; ///< Coordenada y
 };
 
-//Disjoint Set data structure
+/**
+ * Disjoint Set data structure
+ */
 class DisjointSet {
-    std::vector<int> parent, rank;
+    std::vector<int> parent; ///< Vector de padres
+	std::vector<int> rank; ///< Vector de rangos
 public:
     DisjointSet(int n);
+
+	/**
+	 *  Encuentra el padre de un elemento
+	 */
     int find(int x);
+
+	/**
+	 * Une dos elementos
+	 */
     void unite(int x, int y);
 };
 
-std::vector<Edge> kruskalMST(std::vector<Edge>& edges, int V);
+/**
+ * Algoritmo de kruskal
+ */
+std::vector<Edge> kruskal_MST(std::vector<Edge>& edges, int V);
+
+/**
+ * Calcular distancia entre dos puntos
+ */
 double distance(const Point& p1, const Point& p2);
+
+/**
+ * Encontrar la central mas cercana
+ */
 Point findNearestCentral(const std::vector<Point>& centrals, const Point& newPoint);
 
 #endif
-
